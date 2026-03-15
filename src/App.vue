e75e88eb60843aa2bb34979da4549b6c00adeb06
Subashini Ranshika <subashiranshika2002@gmail.com>
2026-03-16 03:43:29 +0530

    Updated App.vue layout and Tailwind styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center">
+  <div class="p-6">
     <h1 class="text-4xl font-bold text-blue-600">
-      My GUI Assignment Project
+      GUI Vue SPA Assignment
     </h1>
+    <p class="mt-4 text-gray-600">
+      Welcome to my Vue Single Page Application project.
+    </p>
   </div>
 </template>
 
