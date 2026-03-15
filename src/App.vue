0be039f144851a05a6267c355ec4976a483136dc
Subashini Ranshika <subashiranshika2002@gmail.com>
2026-03-16 02:50:34 +0530

    Initial Vue SPA project setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600">
+      My GUI Assignment Project
+    </h1>
+  </div>
+</template>
+
+
+
+ 
